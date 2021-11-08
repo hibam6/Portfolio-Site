e1a6d50ab02b184e11ebab8540d30209ece8537a
hibam6 <40648286+hibam6@users.noreply.github.com>
2021-11-08 11:06:10 -0600

    Added toggleable menu on mobile screen sizes

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
-console.log("Hello world")
+function menuToggle() {
+
+  var navObject = document.getElementById('navbarLinks');
+  var hamburger = document.getElementById('hamburger');
+
+  if (navObject.style.display === "flex") {
+    navObject.style.display = "none";
+    hamburger.style.display = "flex";
+  } else {
+    navObject.style.display = "flex";
+    hamburger.style.display = "none";
+  }
+}
